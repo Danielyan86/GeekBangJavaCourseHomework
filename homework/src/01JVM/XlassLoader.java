a573129af75d739dfdb4452bc82e094b2099fc25
Danielyan86 <516495459@qq.com>
2021-05-06 18:23:11 +0800

    add XlassLoader.java

--- homework/src/01JVM/XlassLoader.java
+++ homework/src/01JVM/XlassLoader.java
@@ -3,10 +3,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
 
-/*
- 第一周作业:
- 2.（必做）自定义一个 Classloader，加载一个 Hello.xlass 文件，执行 hello 方法，此文件内容是一个 Hello.class 文件所有字节（x=255-x）处理后的文件。文件群里提供。
- */
 public class XlassLoader extends ClassLoader {
 
     public static void main(String[] args) throws Exception {
@@ -18,6 +14,7 @@ public class XlassLoader extends ClassLoader {
         // 加载相应的类
         Class<?> clazz = classLoader.loadClass(className);
         // 看看里面有些什么方法
+        System.out.println("print the method in class:");
         for (Method m : clazz.getDeclaredMethods()) {
             System.out.println(clazz.getSimpleName() + "." + m.getName());
         }
